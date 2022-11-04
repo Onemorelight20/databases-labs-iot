27a8a803a9ec75eaeb60930d961aa94f796c48b1
Bohdan Boretskyi <bogdanbor.etc@gmail.com>
2022-11-04 23:35:28 +0200

    fix

--- lab-5-spring-data-jpa/src/main/java/ua/boretskyi/Lab5SpringDataJpaApplication.java
+++ lab-5-spring-data-jpa/src/main/java/ua/boretskyi/Lab5SpringDataJpaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Lab5SpringDataJpaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(Lab5SpringDataJpaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Lab5SpringDataJpaApplication.class, args);
+    }
 }
